08d01999cac7fc149ed953586780d53af77def5f
LIM HEE JIN <xeexin0331@gmail.com>
2023-12-26 10:15:41 +0900

    #6.2 Deps
    .

--- src/App.js
+++ src/App.js
@@ -4,13 +4,29 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("I run all the time");
+  const onChange = (event) => setKeyword(event.target.value);
   useEffect(() => {
-    console.log("Call The API...");
+    console.log("I run only once");
   }, []);
+  useEffect(() => {
+    // if (keyword !== "" && keyword.length > 4) {
+    //   console.log("SEARCH FOR :", keyword);
+    // }
+    console.log("I run when 'keyword' changes");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes");
+  }, [counter]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1 className={styles.title}>{counter}</h1>
       <button onClick={onClick}>CLICK</button>
       {/* <Button text={"Continue"} /> */}
