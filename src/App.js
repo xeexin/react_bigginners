492fbe9dddf11acae93e65351486e821cb4c3706
LIM HEE JIN <xeexin0331@gmail.com>
2023-12-27 15:54:39 +0900

    #7.0 To Do List part One
    .

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import Button from "./Button";
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
-function Hello() {
-  useEffect(() => {
-    console.log("Hi :) ");
-    return () => console.log("Bye :(");
-  }, []);
-  return <h1>Hello</h1>;
-}
+
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos : ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your TO DO.."
+        />
+        <button>Add ToDo </button>
+      </form>
     </div>
   );
 }
