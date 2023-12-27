56f43ce7f3fb367fd7b6de8d271c904f06435d2f
LIM HEE JIN <xeexin0331@gmail.com>
2023-12-27 16:05:41 +0900

    #7.1 To Do List part Two
    .

--- src/App.js
+++ src/App.js
@@ -26,6 +26,12 @@ function App() {
         />
         <button>Add ToDo </button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
